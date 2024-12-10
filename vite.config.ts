a0b718e9e6d8d3acc0a32e630e717cad387afc63
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2024-12-10 10:03:06 +0800

    YES

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite"; 
 import react from "@vitejs/plugin-react"; 
 import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
-// https://vitejs.dev/config/ 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: "/trailmarket/",
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [tailwindcss()],
+      plugins: [tailwindcss(), autoprefixer()],
     },
   },
   build: {
